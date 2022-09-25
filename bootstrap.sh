ac09f249f32730d962a5d27cc90bb8cdff2253ac
imm-industry05 <imm-industry05@gmail.com>
2022-09-25 21:33:42 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,23 @@
 
 hostname=`hostname`
 ipv4=`hostname -I`
+jumpserver="192.168.0.250\/32"
+webhook="https://hooks.slack.com/services/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"
+
 
 update_packages() {
     echo -e "[INFO] Updating the machine ${hostname}"
         yum update -y
 
     echo -e "[INFO] Install necessary packages...."
-        yum install -y epel-release git
+        yum install -y epel-release git nfs-utils
+
+    echo -e "[INFO] Cloning bash_scripts configuration"
+        rm -rf bash_scripts
+        git clone https://github.com/imm-industry05/bash_scripts.git bash_scripts
+        sed -i "s/ip hosts/${ipv4} ${hostname}/g"  bash_scripts/hosts
+        cat bash_scripts/hosts > /etc/hosts
+        
 }
 
 tier1() {
@@ -27,19 +37,19 @@ tier1() {
                 echo "[INFO] nginx is not running, starting nginx."
                 systemctl start nginx
         fi
-
-    echo -e "[INFO] Cloning bash_scripts configuration"
-        git clone https://github.com/imm-industry05/bash_scripts.git bash_scripts
     
-    echo -e "[INFO] Denying ALL Hosts...."
-        cat  hosts.deny > /etc/hosts.deny
+    # echo -e "[INFO] Denying ALL Hosts...."
+    #     cat  bash_scripts/hosts.deny > /etc/hosts.deny
 
-    echo -e "[INFO] Allowing ${ipv4} on ${hostname}"
-        sed -e "s/sshd: ipv4/sshd: ${ipv4}/g" hosts.allow
-        cat hosts.allow > /etc/hosts.allow
+    # echo -e "[INFO] Allowing ${jumpserver} on ${hostname}"
+    #     sed -i "s/sshd: ipv4/sshd: ${jumpserver}/g"  bash_scripts/hosts.allow
+    #     cat bash_scripts/hosts.allow > /etc/hosts.allow
+
+    # echo -e "[INFO] Restarting sshd service..."
+    #     systemctl restart sshd
     
-    echo -e  "[INFO] Checkin hosts.allow"
-        cat /etc/hosts.allow
+    # echo -e  "[INFO] Checkin hosts.allow"
+    #     cat /etc/hosts.allow
 
     echo -e "[INFO] Allowing http and https service."
         firewall-cmd --zone=public --permanent --add-service=https
@@ -51,24 +61,24 @@ tier1() {
     
     echo -e "[INFO] Reloading firewall-cmd"
         firewall-cmd --reload
+
+    echo -e "[INFO] Sending notification...."
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"CPE-301: WebServer with hostname: ${hostname} and ip: ${ipv4} is up and running.\"}" ${webhook}
 }
 
 tier2() {
     echo -e "[INFO] Install httpd on ${hostname}"
         yum install -y httpd
-
-    echo -e "[INFO] Cloning bash_scripts configuration"
-        git clone https://github.com/imm-industry05/bash_scripts.git bash_scripts
     
-    echo -e "[INFO] Denying ALL Hosts...."
-        cat  hosts.deny > /etc/hosts.deny
+    # echo -e "[INFO] Denying ALL Hosts...."
+    #     cat  bash_scripts/hosts.deny > /etc/hosts.deny
 
-    echo -e "[INFO] Allowing ${ipv4} on ${hostname}"
-        sed -e "s/sshd: ipv4/sshd: ${ipv4}/g" hosts.allow
-        cat hosts.allow > /etc/hosts.allow
+    # echo -e "[INFO] Allowing ${jumpserver} on ${hostname}"
+    #     sed -i "s/sshd: ipv4/sshd: ${jumpserver}/g"  bash_scripts/hosts.allow
+    #     cat bash_scripts/hosts.allow > /etc/hosts.allow
     
-    echo -e  "[INFO] Checkin hosts.allow"
-        cat /etc/hosts.allow
+    # echo -e  "[INFO] Checkin hosts.allow"
+    #     cat /etc/hosts.allow
 
     echo -e "[INFO] Allowing http and https service."
         firewall-cmd --zone=public --permanent --add-service=https
@@ -76,18 +86,87 @@ tier2() {
 
     echo -e "[INFO] Allowing port 80 and 443."
         firewall-cmd --zone=public --permanent --add-port=80/tcp
+        firewall-cmd --zone=public --permanent --add-port=8080/tcp
         firewall-cmd --zone=public --permanent --add-port=443/tcp
+        firewall-cmd --zone=public --permanent --add-port=8443/tcp
     
     echo -e "[INFO] Reloading firewall-cmd"
         firewall-cmd --reload
+
+    echo -e "[INFO] Sending notification...."
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"CPE-301: Application Server with hostname: ${hostname} and ip: ${ipv4} is up and running.\"}" ${webhook}
 }
 
 tier3() {
     echo -e "[INFO] Installing database server on ${hostname}"
+        yum install -y mariadb-server
+
+    echo -e "[INFO] Set mariadb-server to start on boot"
+        systemctl enable mariadb
+
+    echo -e "[INFO] Starting mariadb-server server"
+        systemctl start mariadb
+    
+    # echo -e "[INFO] Denying ALL Hosts...."
+    #     cat  bash_scripts/hosts.deny > /etc/hosts.deny
+
+    # echo -e "[INFO] Allowing ${jumpserver} on ${hostname}"
+    #     sed -i "s/sshd: ipv4/sshd: ${jumpserver}/g"  bash_scripts/hosts.allow
+    #     cat bash_scripts/hosts.allow > /etc/hosts.allow
+    
+    # echo -e  "[INFO] Checkin hosts.allow"
+    #     cat /etc/hosts.allow
+
+    echo -e "[INFO] Allowing MySQL Ports."
+        firewall-cmd --zone=public --permanent --add-port=3306/tcp
+    
+    echo -e "[INFO] Reloading firewall-cmd"
+        firewall-cmd --reload
+
+    echo -e "[INFO] Sending notification...."
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"CPE-301: Database Server with hostname: ${hostname} and ip: ${ipv4} is up and running.\"}" ${webhook}
 }
 
 nfs() {
     echo -e "[INFO] Installing nfs server on ${hostname}"
+        yum install -y nfs-utils
+    
+    echo -e  "[INFO] Set nfs services to start on boot"
+        systemctl enable rpcbind
+        systemctl enable nfs-server
+        systemctl enable nfs-lock
+        systemctl enable nfs-idmap
+    
+    echo -e  "[INFO] Starting nfs services"
+        systemctl start rpcbind
+        systemctl start nfs-server
+        systemctl start nfs-lock
+        systemctl start nfs-idmap
+
+    # echo -e "[INFO] Cloning bash_scripts configuration"
+    #     rm -rf bash_scripts
+    #     git clone https://github.com/imm-industry05/bash_scripts.git bash_scripts
+    
+    # echo -e "[INFO] Denying ALL Hosts...."
+    #     cat  bash_scripts/hosts.deny > /etc/hosts.deny
+
+    # echo -e "[INFO] Allowing ${jumpserver} on ${hostname}"
+    #     sed -i "s/sshd: ipv4/sshd: ${jumpserver}/g"  bash_scripts/hosts.allow
+    #     cat bash_scripts/hosts.allow > /etc/hosts.allow
+    
+    # echo -e  "[INFO] Checkin hosts.allow"
+    #     cat /etc/hosts.allow
+
+    echo -e "[INFO] Allowing http and https service."
+        firewall-cmd --permanent --zone=public --add-service=nfs
+        firewall-cmd --permanent --zone=public --add-service=mountd
+        firewall-cmd --permanent --zone=public --add-service=rpc-bind
+    
+    echo -e "[INFO] Reloading firewall-cmd"
+        firewall-cmd --reload
+
+    echo -e "[INFO] Sending notification...."
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"CPE-301: NFS Server with hostname: ${hostname} and ip: ${ipv4} is up and running.\"}" ${webhook}
 }
 
 update_packages
