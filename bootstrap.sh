bfe8946164aaadc7f14f15f8338a4a1c8336c334
imm-industry05 <imm-industry05@gmail.com>
2022-09-25 17:49:10 +0800

    Added bash script

---
+++ bootstrap.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -xe
+
+hostname=`hostname`
+ipv4=`hostname -I`
+
+update_packages() {
+    echo -e "[INFO] Updating the machine ${hostname}"
+        yum update -y
+
+    echo -e "[INFO] Install necessary packages...."
+        yum install -y epel-release git
+}
+
+tier1() {
+    echo -e "[INFO] Installing nginx on ${hostname}"
+        yum install -y nginx
+
+    echo -e "[INFO] Starting nginx server"
+        systemctl start nginx
+
+    echo -e "[INFO] Set nginx to start on boot"
+        systemctl enable nginx
+
+    echo -e "[INFO] Cloning nginx configuration"
+        git clone https://github.com/imm-industry05/bash_scripts.git bash_scripts
+    
+    echo -e "[INFO] Denying ALL Hosts...."
+        cat  bash_scripts/hosts.deny > /etc/hosts
+
+    echo -e "[INFO] Allowing ${ipv4} on ${hostname}"
+        sed -e "s/sshd: ipv4/sshd: '${ipv4}'/g" bash_scripts/hosts.allow
+
+}
+
+tier2() {
+    echo -e "[INFO] Install httpd on ${hostname}"
+}
+
+tier3() {
+    echo -e "[INFO] Installing database server on ${hostname}"
+}
+
+nfs() {
+    echo -e "[INFO] Installing nfs server on ${hostname}"
+}
+
+update_packages
+
+case ${hostname} in
+
+    tier1-webserver.local.com)
+        tier1
+        ;;
+    tier2-app1.local.com || tier2-app2.local.com)
+        tier2
+        ;;
+    tier3-db.local.com)
+        tier3
+        ;;
+    nfs.local.com)
+        nfs
+        ;;
+        *)
+        echo -e "[INFO] No command..."
+
+esac
